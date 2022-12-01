f204da187b7d90b951102e5e8724d62c724d8c5e
fantonioluz <falf@cesar.school>
2022-12-01 15:14:25 -0300

    Sidebar

--- components/sidebar.py
+++ components/sidebar.py
@@ -17,8 +17,119 @@ import pandas as pd
 
 # ========= Layout ========= #
 layout = dbc.Col([
+                html.H1("Tecnowall", className="text-primary"),
+                html.P("Finanças", className="text-info"),
+                html.Hr(),
                 
-            ])
+    #Seção Perfil --------------------
+                dbc.Button(id='botao_avatar',
+                    children=[html.Img(src='/assets/img_hom.png', id='avatar_change', alt='Avatar', className='perfil_avatar')
+                    ], style={'background-color': 'transparent', 'border-color':'transparent'}),
+           
+    #Seção Botoes --------------------
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Button(color='success',id='open-novo-receita',
+                        children=['+ Receita'])
+                    ],width=6),
+                    dbc.Col([
+                        dbc.Button(color='danger',id='open-novo-despesa',
+                        children=['+ Despesa'])
+                    ],width=6)
+                ]),    
+
+                #Modal receita
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adcionar Receita')),
+                    dbc.ModalBody([
+                        dbc.Row([
+                            dbc.Col([
+                                dbc.Label('Descrição: '),
+                                dbc.Input(placeholder="Ex.: dividendos da bolsa, herança...", id="txt-receita")
+                            ], width=6),
+                            dbc.Col([
+                                dbc.Label("Valor: "),
+                                dbc.Input(placeholder="$100.00",id="valor_receita", value="")
+                            ],width=6)
+                        ]),
+                        dbc.Row([
+                            dbc.Col([ 
+                                dbc.Label("Data: "),
+                                dcc.DatePickerSingle(id='date-receitas',
+                                min_date_allowed=date(2020,1,1),
+                                max_date_allowed=date(2030,12,31),
+                                date=datetime.today(),
+                                style={"width":"100%"})
+                            ],width=4),
+
+                            dbc.Col([
+                                dbc.Label("Extras"),
+                                dbc.Checklist(
+                                    options=[],
+                                    value=[],
+                                    id='switch-input-recetia',
+                                    switch=True
+                                )
+                            ]),
+                            dbc.Col([
+                                html.Label('Categoria da receita'),
+                                dbc.Select(id='select_receita', options=[], value=[])
+                            ],width=4)
+
+
+
+
+
+
+                        ], style={'margin-top': '25px'}),
+                        dbc.Row(
+                            dbc.Accordion([ 
+                                dbc.AccordionItem(children=[
+                                    dbc.Row([
+                                        dbc.Col([ 
+                                            html.Legend("Adcionar categoria", style={'color':'green'}),
+                                            dbc.Input(type="text", placeholder="Nova categoria...", id="input-add-receita", value=""),
+                                            html.Br(),
+                                            dbc.Button("Adcionar", className="btn btn-success", id="add-category-receita", style={"margin-top": "20px"}),
+                                            html.Br(),
+                                            html.Div(id="category-div-add-receita", style={}),
+                                        ], width=6),
+                                        dbc.Col([ 
+                                            html.Legend('Excluir categoris', style={'color': 'red'}),
+                                            dbc.Checklist(
+                                                id='checklist-selected-style-receita',
+                                                options=[],
+                                                value=[],
+                                                label_checked_style={'color': 'red'},
+                                                input_checked_style={'backgroundColor': 'blue', 'borderColor': 'orange'}
+                                            ),
+                                            dbc.Button('Remover', color='warning', id='remove-category-receita',style={'margin-top': '20px'})
+                                        ],width=6)
+                                    ])
+                                ])
+                            ])
+                        )
+
+
+
+
+
+                    ])
+                ], id='modal-novo-receita'),
+                #Modal Despasa
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adcionar Despesa')),
+                    dbc.ModalBody([])
+                ], id='modal-novo-despesa'),
+    #Seção NAV ----------------------
+                html.Hr(),
+                dbc.Nav([
+                    dbc.NavLink("Dashboard", href="/dashboards", active="exact"),
+                    dbc.NavLink("Extratos", href="/extratos", active="exact")
+                ], vertical=True, pills=True, id='nav_buttons', style={"margin-bottom":"50px"}),
+
+
+            ], id='sidebar_completa')
 
 
 
@@ -26,3 +137,20 @@ layout = dbc.Col([
 
 # =========  Callbacks  =========== #
 # Pop-up receita
+@app.callback(
+    Output('modal-novo-receita', 'is_open'),
+    Input('open-novo-receita', 'n_clicks'),
+    State('modal-novo-receita', 'is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
+# Pop-up despesa
+@app.callback(
+    Output('modal-novo-despesa', 'is_open'),
+    Input('open-novo-despesa', 'n_clicks'),
+    State('modal-novo-despesa', 'is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open        
--- myindex.py
+++ myindex.py
@@ -6,6 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
+from components import sidebar, dashboards, extratos
 
 
 
@@ -15,15 +16,25 @@ content = html.Div(id="page-content")
 
 
 app.layout = dbc.Container(children=[
-
-
-
-
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'),
+            sidebar.layout 
+        ], md=2),
+        dbc.Col([
+            content
+        ], md=10)
+    ])
 
 ], fluid=True,)
 
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def render_page(pathname):
+    if pathname == '/' or pathname == '/dashboards':
+        return dashboards.layout
 
-
+    if pathname == '/extratos':
+        return extratos.layout
 
 if __name__ == '__main__':
     app.run_server(port=8051, debug=True)
